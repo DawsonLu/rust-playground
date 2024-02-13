e85b44f2805f44817d5720d22286d1127fbff2e2
Dawson Lu <dawson.lu@stryten.com>
2024-02-13 16:11:55 -0500

    Added hello-rust program

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\lud\OneDrive - stryten.com\Desktop\Github\RustPlayground\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\Users\lud\OneDrive - stryten.com\Desktop\Github\RustPlayground\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\lud\OneDrive\ -\ stryten.com\Desktop\Github\RustPlayground\hello-rust\target\debug\hello-rust.exe: C:\Users\lud\OneDrive\ -\ stryten.com\Desktop\Github\RustPlayground\hello-rust\src\main.rs
