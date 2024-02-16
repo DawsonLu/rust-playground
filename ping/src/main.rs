57a445a0e6ebf1e22329f03d14142b01fdd04a73
Dawson Lu <dawson.lu@stryten.com>
2024-02-16 09:44:25 -0500

    Added ping

--- ping/src/main.rs
+++ ping/src/main.rs
@@ -1,50 +1,40 @@
 extern crate pnet;
 
 use pnet::packet::icmp::{echo_request::MutableEchoRequestPacket, IcmpTypes};
-use pnet::packet::Packet;
+use pnet::packet::{MutablePacket, Packet};
 use pnet::transport::{icmp_packet_iter, transport_channel, TransportChannelType::Layer3};
-use std::net::{IpAddr, Ipv4Addr};
-use std::time::Duration;
-use std::io;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
 
 fn main() {
-    println!("Please enter an IP Address:");
-
-    let mut target_ip = String::new();
-
-    io::stdin().read_line(&mut target_ip)
-        .expect("Failed to read line");
-
-    let target: IpAddr = target_ip.parse().expect("Invalid IP Address");
-
-    // Creating a transport channel to send and receive ICMP packets.
-    let (mut tx, mut rx) = match transport_channel(1024, Layer3(IcmpTypes::EchoRequest)) {
+    let protocol = Layer3(IcmpTypes::EchoRequest);
+    let (mut tx, mut rx) = match transport_channel(1024, protocol) {
         Ok((tx, rx)) => (tx, rx),
-        Err(e) => panic!("Error creating the transport channel: {}", e),
+        Err(e) => panic!("An error occurred when creating the transport channel: {}", e),
     };
+
+    println!("Please enter an IP address to ping:");
+    let mut ip_address = String::new();
+    std::io::stdin().read_line(&mut ip_address).expect("Failed to read line");
+    let ip_address = ip_address.trim(); // Remove newline character
+    let ip = Ipv4Addr::from_str(ip_address).expect("Invalid IP address");
 
     let mut buffer = [0u8; 64];
     let mut packet = MutableEchoRequestPacket::new(&mut buffer).unwrap();
     packet.set_sequence_number(1);
     packet.set_identifier(0);
-    let checksum = pnet::packet::icmp::checksum(&packet.to_immutable());
-    packet.set_checksum(checksum);
+    packet.set_icmp_type(IcmpTypes::EchoRequest);
+    packet.set_checksum(pnet::util::checksum(packet.packet(), 1));
 
-    println!("Sending ICMP echo request to {}", target);
-
-    match tx.send_to(packet, target) {
-        Ok(_) => println!("Packet sent."),
-        Err(e) => println!("Failed to send packet: {}", e),
+    match tx.send_to(packet, ip) {
+        Ok(_) => println!("Sent an echo request to {}", ip),
+        Err(e) => println!("Failed to send echo request: {}", e),
     }
 
-    let (mut iterator, _) = icmp_packet_iter(&mut rx);
-    let timeout = Duration::from_secs(1);
-
-    match iterator.next_with_timeout(timeout) {
-        Ok(Some((packet, _))) if packet.get_icmp_type() == IcmpTypes::EchoReply => {
-            println!("Received ICMP echo reply from {}", target);
-        }
-        Ok(_) => println!("No ICMP echo reply received."),
-        Err(e) => println!("Failed to receive packet: {}", e),
+    let mut iter = icmp_packet_iter(&mut rx);
+    let (packet, _) = iter.next().expect("Failed to receive packet");
+    match packet.get_icmp_type() {
+        IcmpTypes::EchoReply => println!("Received an echo reply from {}", ip),
+        _ => println!("Received a different type of ICMP packet"),
     }
 }
---
+++ ping/target/debug/ping.d
@@ -0,0 +1,1 @@
+C:\Users\lud\OneDrive\ -\ stryten.com\Desktop\Github\RustPlayground\ping\target\debug\ping.exe: C:\Users\lud\OneDrive\ -\ stryten.com\Desktop\Github\RustPlayground\ping\src\main.rs
